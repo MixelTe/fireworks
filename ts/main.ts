f77d6261e56bab23e030259bc77aaad0bc5ae338
Mike <63655075+MixelTe@users.noreply.github.com>
2023-12-31 14:16:09 +0300

    fireworks

--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,6 @@
-import { Particle, createParticle, drawParticle, loopParticle, moveParticle } from "./particles.js";
+import { Particle, createParticle, drawParticle, loopParticle, calcParticle } from "./particles.js";
 import * as Lib from "./littleLib.js";
+import { startFireworks } from "./fireworks.js";
 
 const date = Lib.get.el("date", HTMLSpanElement);
 const title = Lib.get.div("title");
@@ -8,10 +9,32 @@ const ctx = Lib.canvas.getContext2d(canvas);
 let lastT = 0;
 let lastSnowflake = 0;
 let animateBorder = -2;
+let stop = false;
+let removeSnowflakes = false;
 const snowflakes: Particle[] = [];
 
+// // TEMP
+// const overlay = document.querySelector(".overlay");
+// if (overlay && overlay.parentElement) overlay.parentElement.removeChild(overlay);
+// startFireworks();
+// // TEMP
+
+Lib.addButtonListener("btn-start", () =>
+{
+	removeSnowflakes = true;
+	animateBorder = -3;
+	document.body.classList.add("animateBorder");
+	document.body.classList.add("hide");
+	setTimeout(() =>
+	{
+		const overlay = document.querySelector(".overlay");
+		if (overlay && overlay.parentElement) overlay.parentElement.removeChild(overlay);
+		startFireworks();
+	}, 1300);
+});
+
 setTimeout(() => document.body.classList.remove("hidden"), 10);
-setTimeout(() => animateBorder = -1, 3300);
+setTimeout(() => animateBorder == -2 ? animateBorder = -1 : {}, 3300);
 draw(0)
 function draw(t: number)
 {
@@ -25,23 +48,43 @@ function draw(t: number)
 		document.body.classList.remove("animateBorder");
 	}
 	if (animateBorder > 0)
-		title.style.setProperty("--block-borderC", Lib.hslColor(Math.floor(((t - animateBorder) / 100 + 9) % 360 * 10) / 10, 100, 64));
+		title.style.setProperty("--block-borderC", Lib.hslColor(Math.floor(((t - animateBorder) / 100 + 210) % 360 * 10) / 10, 100, 64));
+	if (animateBorder == -3)
+		title.style.setProperty("--block-borderC", "");
 
 	Lib.canvas.fitToParent(canvas);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	if (t - lastSnowflake > 100 && snowflakes.length < 100)
+	if (!removeSnowflakes && t - lastSnowflake > 100 && snowflakes.length < canvas.width * canvas.height / 10000)
 	{
-		snowflakes.push(createParticle([0, canvas.width], 0, [2, 5], [-0.5, 0.5], [0.1, 0.2], "white", 0.5));
+		snowflakes.push(createParticle({
+			x: [0, canvas.width],
+			y: 0,
+			r: [2, 5],
+			dx: [-0.5, 0.5],
+			dy: [0.1, 0.2],
+			c: "white",
+			a: 0.5,
+		}));
 		lastSnowflake = t;
 	}
 
-	for (const snowflake of snowflakes)
+	for (let i = snowflakes.length - 1; i >= 0; i--)
 	{
-		moveParticle(snowflake, dt);
-		loopParticle(snowflake, canvas.width, canvas.height);
+		const snowflake = snowflakes[i];
+		calcParticle(snowflake, dt);
+		const { loop } = loopParticle(snowflake, canvas.width, canvas.height);
+		if (removeSnowflakes && loop)
+		{
+			snowflakes.splice(i, 1);
+			if (snowflakes.length == 0)
+				stop = true;
+		}
 		drawParticle(ctx, snowflake);
 	}
 
-	requestAnimationFrame(draw);
+	if (stop)
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	else
+		requestAnimationFrame(draw);
 }
--- ts/particles.ts
+++ ts/particles.ts
@@ -1,21 +1,21 @@
-import { randomNum } from "./littleLib.js";
+import { hslColor, randomNum } from "./littleLib.js";
 
-export function createParticle(xr: Range | number, yr: Range | number, rr: Range | number, dxr: Range | number, dyr: Range | number, c: string, a: number): Particle
+export function createParticle(p: RandomParticle): Particle
 {
-	if (typeof xr == "number") xr = [xr, xr];
-	if (typeof yr == "number") yr = [yr, yr];
-	if (typeof rr == "number") rr = [rr, rr];
-	if (typeof dxr == "number") dxr = [dxr, dxr];
-	if (typeof dyr == "number") dyr = [dyr, dyr];
-
 	return {
-		x: randomNum(...xr),
-		y: randomNum(...yr),
-		r: randomNum(...rr),
-		dx: randomNum(...dxr),
-		dy: randomNum(...dyr),
-		c,
-		a,
+		x: typeof p.x == "number" ? p.x : randomNum(...p.x),
+		y: typeof p.y == "number" ? p.y : randomNum(...p.y),
+		r: typeof p.r == "number" ? p.r : randomNum(...p.r),
+		a: typeof p.a == "number" ? p.a : randomNum(...p.a),
+		c: typeof p.c == "string" ? p.c : hslColor(
+			typeof p.c[0] == "number" ? p.c[0] : randomNum(...p.c[0]),
+			typeof p.c[1] == "number" ? p.c[1] : randomNum(...p.c[1]),
+			typeof p.c[2] == "number" ? p.c[2] : randomNum(...p.c[2]),
+		),
+		dx: p.dx == undefined ? 0 : typeof p.dx == "number" ? p.dx : randomNum(...p.dx),
+		dy: p.dy == undefined ? 0 : typeof p.dy == "number" ? p.dy : randomNum(...p.dy),
+		dr: p.dr == undefined ? 0 : typeof p.dr == "number" ? p.dr : randomNum(...p.dr),
+		da: p.da == undefined ? 0 : typeof p.da == "number" ? p.da : randomNum(...p.da),
 	};
 }
 
@@ -30,18 +30,39 @@ export function drawParticle(ctx: CanvasRenderingContext2D, particle: Particle)
 	ctx.restore();
 }
 
-export function moveParticle(particle: Particle, dt: number)
+export function calcParticle(particle: Particle, dt: number)
 {
 	particle.x += particle.dx * dt;
 	particle.y += particle.dy * dt;
+	particle.r += particle.dr * dt;
+	particle.a += particle.da * dt;
 }
 
 export function loopParticle(particle: Particle, w: number, h: number)
 {
-	if (particle.x - particle.r > w) particle.x = particle.x % w;
-	if (particle.x + particle.r < 0) particle.x = (particle.x % w) + w;
-	if (particle.y - particle.r > h) particle.y = particle.y % h;
-	if (particle.y + particle.r < 0) particle.y = (particle.y % h) + h;
+	let loopX = 0;
+	let loopY = 0;
+	if (particle.x - particle.r > w)
+	{
+		particle.x = particle.x % w;
+		loopX = 1;
+	}
+	if (particle.x + particle.r < 0)
+	{
+		particle.x = (particle.x % w) + w;
+		loopX = -1;
+	}
+	if (particle.y - particle.r > h)
+	{
+		particle.y = particle.y % h;
+		loopY = 1;
+	}
+	if (particle.y + particle.r < 0)
+	{
+		particle.y = (particle.y % h) + h;
+		loopY = -1;
+	}
+	return { loop: loopX != 0 || loopY != 0, loopX, loopY };
 }
 
 type Range = [number, number];
@@ -51,8 +72,23 @@ export interface Particle
 	x: number;
 	y: number;
 	r: number;
+	a: number;
+	c: string;
 	dx: number;
 	dy: number;
-	c: string;
-	a: number;
+	dr: number;
+	da: number;
+}
+
+export interface RandomParticle
+{
+	x: number | Range;
+	y: number | Range;
+	r: number | Range;
+	a: number | Range;
+	c: string | [number | Range, number | Range, number | Range];
+	dx?: number | Range;
+	dy?: number | Range;
+	dr?: number | Range;
+	da?: number | Range;
 }
